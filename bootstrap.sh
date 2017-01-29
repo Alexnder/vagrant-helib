aafe4554b1b71c5d1332c5c4cc6b4a19ee9dc3ac
Alexnder <opensunset@gmail.com>
2017-01-30 02:21:04 +0300

    Fixed "Segmentation fault"

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,14 @@
 apt-get update
 apt-get install -y m4 gcc make build-essential linux-headers-$(uname -r) git
 
-curl https://gmplib.org/download/gmp/gmp-6.1.1.tar.bz2 | tar -xj
+curl -s https://gmplib.org/download/gmp/gmp-6.1.1.tar.bz2 | tar -xj
 cd gmp-6.1.1
 ./configure && make && make install || (echo "gmp install error" && exit -1)
 cd ..
 
 
-curl http://www.shoup.net/ntl/ntl-10.3.0.tar.gz | tar -xz
-cd ntl-10.3.0/src
+curl -s http://www.shoup.net/ntl/ntl-9.11.0.tar.gz | tar -xz
+cd ntl-9.11.0/src
 ./configure NTL_GMP_LIP=on && make && make install || (echo "ntl install error" && exit -1)
 cd ../..
 
